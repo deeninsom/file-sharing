cbd22a5fccfd00b43f872a54c01fb4267a53db04
Muhammad Syihabuddin <syihab855@gmail.com>
2024-02-12 21:16:29 +0700

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "jodi",
+    "password": process.env.DB_PASSWORD,
     "database": "db_file_shared",
     "host": "localhost",
     "dialect": "mysql"
